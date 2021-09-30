8602347a74c259d402b9d28f3e7e422ac744651f
Isabella <imkenton@cpp.edu>
2021-09-29 18:40:57 -0700

    Initial commit + outline for 1) ResturantBill

---
+++ bill.c
@@ -0,0 +1,23 @@
+/*
+Outline:
+Resturant Bill:
+1. create an void main function that takes in 2 doubles(tax and tip percentage)
+
+2. create 4 meal costs as doubles
+double salad = 9.95;
+double soup = 4.55;
+double sandwich 12.25;
+double pizza = 22.35;
+
+3. create a random method to select a meal cost. Maybe associate each meal with a number value 0-3. create an if statement to calculate that.
+
+4. calculate total bill cost. 
+tip =/ 100;
+tip=*mealcost;
+tax =/ 100;
+tax =* mealcost;
+total = mealcost + tax + tip
+
+5. print out meal, tax, tip and total bill
+*/
+
