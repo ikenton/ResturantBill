515758382780bb5130c66c14dcd74523cf4fd11d
Isabella <imkenton@gmail.com>
2021-09-29 19:15:49 -0700

    Created a main method, and included 4 meal costs

--- bill.c
+++ bill.c
@@ -6,7 +6,7 @@ Resturant Bill:
 2. create 4 meal costs as doubles
 double salad = 9.95;
 double soup = 4.55;
-double sandwich 12.25;
+double sandwich = 12.25;
 double pizza = 22.35;
 
 3. create a random method to select a meal cost. Maybe associate each meal with a number value 0-3. create an if statement to calculate that.
@@ -21,3 +21,12 @@ total = mealcost + tax + tip
 5. print out meal, tax, tip and total bill
 */
 
+#include <stdio.h>
+
+void main(int tax, int tip){
+    double salad = 9.95;
+    double soup = 4.55;
+    double sandwich =12.25;
+    double pizza = 22.35;
+    
+}
